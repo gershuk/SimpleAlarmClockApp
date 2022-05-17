d37975f17e4da3b8afe932318877587664eb7bae
Vladislav <vladik.morozov.2012@mail.ru>
2022-05-17 17:11:06 +1000

    Add project files.

---
+++ defines/AppDefines.js
@@ -0,0 +1,10 @@
+class AppDefines {
+    static TitleHomeScreen = "Alarm";
+    static TitleCreateionScreen = "Create alarm";
+    static TitleRadioChoose = "Choose a radio:";
+    static TitleNotification = "Alarm notification!";
+    static LabelPlayingRadio = "Playing radio:";
+    static LabelDisableAlarm = "Dismiss";
+}
+
+export default AppDefines;
